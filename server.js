9ddca6a19c9e93cdd034a77113396e0befc6d48a
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-02-25 14:26:32 -0500

    edit server code based on challenge instructions

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+// On the back end, the application should include a db.json file that will be used to store and retrieve notes using the fs module.
+const fs = require('fs');
 const notesData = require('./db/db.json');
 
 const app = express();
@@ -9,22 +11,25 @@ const PORT = 3001;
 app.use(express.static('public')); // Makes the route "/" index.html by defult
 
 // Not really needed since the above is doing the same: (keeping it for now to keep track of route paths visually in the code)
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/index.html'))
+// );
 
-// Sends data as an html file (front-end)
+// HTML route: GET /notes should return the notes.html file
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-// Sends data as a json file database (back-end)
-app.get('/api/notes', (req, res) => { // Change route because same method (get) cannot have same route (using api path as it is commonly known route for backend database)
+// API route: GET /api/notes sould read the db.json file and return all saved notes as JSON.
+app.get('/api/notes', (req, res) => {
   res.json(notesData);
 });
 
-// If user inputs a route that does not exist in this application, it will send the user to the main page
-app.get('*', (req, res) =>
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+
+// HTML route: GET * should return the index.html file.
+app.get('*', (req, res) => // If user inputs a route that does not exist in this application, it will send the user to the main page
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
